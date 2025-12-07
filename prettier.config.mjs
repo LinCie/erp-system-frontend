eb4feca5cf83d036b8332add95ec81804c3884f9
LinCie <aldiofernanda@protonmail.com>
2025-12-07 13:36:57 +0700

    chore: add code quality and git hooks tooling

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
